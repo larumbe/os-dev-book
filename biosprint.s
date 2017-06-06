6811c2b2870d082841185e87994efadfb45c342c
adrianlarumbe <adrian.martinezlarumbe@imgtec.com>
2017-06-06 17:53:27 +0100

    First commit

---
+++ biosprint.s
@@ -0,0 +1,21 @@
+;; printing a message to the screen using BIOS
+	
+	mov ah, 0x0e
+
+	mov al, 'H'
+	int 0x10
+	mov al, 'e'
+	int 0x10
+	mov al, 'l'
+	int 0x10
+	mov al, 'l'
+	int 0x10
+	mov al, 'o'
+	int 0x10
+
+	jmp $
+
+	;; Padding and magic BIOS number
+
+	times 510 - ($-$$) db 0
+	dw 0xaa55
